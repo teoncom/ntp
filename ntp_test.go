1c53d55332b324213b9d53b32319948db173ddc4
Hank Li <HankLi0130@gmail.com>
2024-10-29 14:08:26 +0800

    Update ntp_test.go

--- ntp_test.go
+++ ntp_test.go
@@ -6,12 +6,24 @@ import (
 	"time"
 )
 
-func TestGetTime(t *testing.T) {
-	remoteTime, err := Time("time.google.com")
-	if err != nil {
-		log.Fatal(err)
+func TestTime(t *testing.T) {
+	timeServers := []string{
+		"pool.ntp.org",
+		"clock.isc.org",
+		"time.windows.com",
+		"time.nist.gov",
+		"ntp.nsu.ru",
+		"ntp.ntsc.ac.cn",
 	}
 
-	log.Println("remote time: " + remoteTime.UTC().Format(time.DateTime))
-	log.Println("local time: " + time.Now().UTC().Format(time.DateTime))
+	for _, server := range timeServers {
+		remoteTime, err := Time(server)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("remote time: " + remoteTime.UTC().Format(time.DateTime))
+		log.Println("local time:  " + time.Now().UTC().Format(time.DateTime))
+		log.Println("----------")
+	}
 }
